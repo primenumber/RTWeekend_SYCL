6ce90a7b2e3dfaba57ea04e881b7f45feedf81b1
primenumber <prime@kmc.gr.jp>
2023-05-04 00:13:45 +0900

    Parallelize by SYCL

--- src/main.cpp
+++ src/main.cpp
@@ -30,19 +30,22 @@ int main() {
   const size_t image_height = 256;
   const size_t image_width = 256;
   Image image(image_height, image_width);
-  size_t offset = 0;
-  for (int j = image_height - 1; j >= 0; --j) {
-    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-    for (int i = 0; i < image_width; ++i) {
-      auto r = double(i) / (image_width - 1);
-      auto g = double(j) / (image_height - 1);
-      auto b = 0.25;
+  auto image_buf = sycl::malloc_shared<color>(image_height * image_width, q);
+  q.submit([&](sycl::handler &h) {
+     h.parallel_for(sycl::range<2>(image_height, image_width), [=](auto &idx) {
+       size_t i = image_height - idx[0] - 1;
+       size_t j = idx[1];
+       auto r = float(j) / (image_width - 1);
+       auto g = float(i) / (image_height - 1);
+       auto b = 0.25;
 
-      image.data[offset] = color(r, g, b);
-      ++offset;
-    }
+       size_t offset = idx[0] * image_width + idx[1];
+       image_buf[offset] = color(r, g, b);
+     });
+   }).wait();
+  for (size_t i = 0; i < image_height * image_width; ++i) {
+    image.data[i] = image_buf[i];
   }
-  std::cerr << "\nDone.\n";
   std::cout << image;
   return 0;
 }
